312df07434d6f2b863523eef653dabde09ed77ee
Taichi Uragami <backpaper0@gmail.com>
2018-11-19 08:52:43 +0900

    Initial commit

---
+++ src/main/java/com/example/springsecurityoauth2example/SpringSecurityOauth2ExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.springsecurityoauth2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth2ExampleApplication.class, args);
+	}
+}
